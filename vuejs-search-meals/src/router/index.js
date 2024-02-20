94a59f6deea65ea1b3a828bdae690dc5f4930a88
Jenniline <jennilineebai@gmail.com>
2024-02-20 16:34:24 +0100

    Work In Progress: Search page for Searching meals by name. Added Images and other data to the search result

---
+++ vuejs-search-meals/src/router/index.js
@@ -0,0 +1,52 @@
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router' 
+import  Home  from '../views/Home.vue';
+import  DefaultLayout  from '../components/DefaultLayout.vue';
+import  GuestLayout  from '../components/GuestLayout.vue';
+import  MealsByLetter  from '../views/MealsByLetter.vue';
+import  MealsByName  from '../views/MealsByName.vue';
+import  MealsByIngredient from '../views/MealsByIngredient.vue'
+
+
+const routes = [
+    {
+        path: '/',
+        component: DefaultLayout,
+        children : [
+
+            {
+                path: '/',
+                name: 'home',
+                component: Home
+            },
+            {
+                path: '/by-name/:name?',
+                name: 'byName',
+                component: MealsByName,
+            },  
+            {
+                path: '/by-letter/:letter?',
+                name: 'byLetter',
+                component: MealsByLetter,
+            },  
+            {
+                path: '/by-ingredient/:ingredient?',
+                name: 'byIngredient',
+                component: MealsByIngredient,
+            },
+
+        ] 
+    },
+    {
+        path: '/guest',
+        component: GuestLayout,
+        
+    }
+
+];
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+});
+
+export default router;
